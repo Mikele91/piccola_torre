c51f556cceea1c6b11b40788e25c404ccc945057
Mikele91 <mke_8@hotmail.it>
2023-02-17 17:36:43 +0100

    add

--- src/components/microComponents/Maps.vue
+++ src/components/microComponents/Maps.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="map row">
-        <div class="col-md-8 map_link">
+        <div class="col-md-8 map_link pb-2">
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3000.107536822371!2d16.502432715652432!3d41.24121541300644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1338070ebd338c5f%3A0x199f7844e1ff39a9!2sLa%20Piccola%20Torre!5e0!3m2!1sit!2sit!4v1656588579598!5m2!1sit!2sit" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
